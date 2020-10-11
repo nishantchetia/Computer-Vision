6de0d9213b5456cbe6fdcb6eaa32227691591e33
nishantchetia <62946542+nishantchetia@users.noreply.github.com>
2020-10-11 16:44:37 +0200

    Update task1_1.py

--- task1_1.py
+++ task1_1.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy as np     # To staqck the frames horizontally
+import numpy as np     # To stack the frames horizontally
 
 video = cv2.VideoCapture(0)
 x = 0
@@ -9,13 +9,13 @@ while True:
 
 
 
-    # Fliping the image as said in question
+    # Fliping the image vertically
     vid_flip = cv2.flip(frame,0)
 
-    # Combining the two different image frames in one window
+    # getting the images in one window
     combined_window = np.hstack([frame,vid_flip])
 
-    # Displaying the single window
+    # portraying the window
     cv2.imshow("Combined videos ",combined_window)
     key=cv2.waitKey(1)
 

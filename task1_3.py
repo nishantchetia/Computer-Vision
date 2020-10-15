04cee946e9c81f855e2b7c1391d51a8542181a38
nishantchetia <62946542+nishantchetia@users.noreply.github.com>
2020-10-15 12:41:24 +0200

    Update task1_3.py

--- task1_3.py
+++ task1_3.py
@@ -10,8 +10,6 @@ print(cap.isOpened())
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
-       print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-       print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
        out.write(frame)
 

5c6c9d818e7b40af6f294f22fe67ac361fbd3e5d
nishantchetia <62946542+nishantchetia@users.noreply.github.com>
2020-10-20 15:31:30 +0200

    Update task1_3.py

--- task1_3.py
+++ task1_3.py
@@ -3,22 +3,20 @@ import cv2
 cap = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
-
+n = int(input(print("Enter the number of frames after which flip needed:")))
 count = 0
-flipped = False
 print(cap.isOpened())
 while (cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
         out.write(frame)
 
-        if not flipped:
+        if count%n != 0:
             cv2.imshow('frame', frame)
-        elif flipped:
+        else:
             frame = cv2.flip(frame,0)
             cv2.imshow('frame', frame)
-        if count % 5 == 0:
-            flipped = not flipped
+
         count += 1
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
